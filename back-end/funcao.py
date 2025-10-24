ef08bf0c15e24060072b08e59938569c1c82f14b
Paulo Ricardo <pauloricardo.st1812@hotmail.com>
2025-10-24 13:57:11 -0300

    função listar filmes

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -20,3 +20,36 @@ def criar_tabela():
         finally:
             cursor.close()
             conexao.close()
+
+def cadastrar_filme(titulo, genero,  ano, nota):
+    conexao, cursor = conector()
+    if conexao:
+        try:
+            cursor.execute(
+            "INSERT INTO filme (titulo, genero, ano, nota) "
+            "VALUES (%s, %s, %s, %s)", (titulo, genero, ano, nota)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao cadastrar filme: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+cadastrar_filme("Interstellar", "Ficção Científica", 2014, 8.6)
+
+def listar_filmes():
+    conexao, cursor = conector()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM filme ORDER BY ID"
+            )
+            for linha in cursor.fetchall():
+                print(linha)
+        except Exception as erro:
+            print(f"Erro ao listar filmes: {erro}")
+            return []
+        finally:
+            cursor.close()
+            conexao.close()
+listar_filmes()
